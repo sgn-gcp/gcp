d4a6f9ed0942f3e492aede71e89e448cbdb931b6
adamsinnottsgn <adam.sinnott@sgn.co.uk>
2020-08-20 15:48:59 +0100

    Commiting additional files related to setting up the basic Amplify Authentication app.

--- gc-log-book/src/App.js
+++ gc-log-book/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
-import Main from './components/Main'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+import Header from './Components/Header'
+import Main from './Components/Main'
+
+Amplify.configure(awsconfig)
 
 function App() {
   return (
     <div>
+      <AmplifySignOut />
       <Header />
       <Main />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
